7d6517a59532c2b71d32064d4a0146f682a292ad
lucio <luciomss@gmail.com>
2022-11-15 10:25:48 -0300

    Integrando frontend com api
    Integrando frontend com api

---
+++ GeekShooping/GeekShooping.web/Controllers/ProdutoController.cs
@@ -0,0 +1,21 @@
+﻿using GeekShooping.web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShooping.web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly IServiceProduto _serviceProduto;
+
+        public ProdutoController(IServiceProduto serviceProduto)
+        {
+            _serviceProduto = serviceProduto ?? throw new ArgumentNullException(nameof(serviceProduto));
+        }
+
+        public async Task<IActionResult> ProdutoIndex()
+        {
+            var produtos = _serviceProduto.FindAllProdutos();
+            return View(produtos);
+        }
+    }
+}
