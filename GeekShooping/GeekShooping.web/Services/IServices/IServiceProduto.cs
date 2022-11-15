b5b0e58c6dba16ae7ad21cd37d55d18a0b29749e
lucio <luciomss@gmail.com>
2022-11-14 22:29:56 -0300

    Consumo api
    Consumo api

---
+++ GeekShooping/GeekShooping.web/Services/IServices/IServiceProduto.cs
@@ -0,0 +1,13 @@
+﻿using GeekShooping.web.Models;
+
+namespace GeekShooping.web.Services.IServices
+{
+    public interface IServiceProduto
+    {
+        Task<IEnumerable<ProdutoModel>> FindAllProdutos();
+        Task<ProdutoModel> FindProdutoById(long id);
+        Task<ProdutoModel> CreateProduto(ProdutoModel model);
+        Task<ProdutoModel> UpdateProduto(ProdutoModel model);
+        Task<bool> DeleteProdutoById(long id);
+    }
+}
